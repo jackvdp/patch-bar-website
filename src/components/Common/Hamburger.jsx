652e74037ca486d1f2069ae825b68cdedbb03767
jackvdp <jack@pumpymusic.co.uk>
2024-03-29 23:41:27 +0000

    Improved sync of hamburger using jquery

--- src/components/Common/Hamburger.jsx
+++ src/components/Common/Hamburger.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Hamburger.module.css';
 
 const NavbarToggleButton = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleHamburger = () => {
-        setIsOpen(!isOpen);
-    };
+    useEffect(() => {
+        const menuIcon = document.querySelector(".topnav .menu-icon");
+        if (menuIcon) {
+            menuIcon.addEventListener("click", function () {
+                document.querySelector(".topnav .menu-icon .text ").classList.contains("open") ? setIsOpen(false) : setIsOpen(true);
+            })
+        }
+    }, []);
 
     return (
         <div className={`${styles.setStyles}`}>
-            <div className={`${styles.navToggle} ${isOpen ? "open" : ''}`} onClick={toggleHamburger} >
+            <div className={`${styles.navToggle} ${isOpen ? "open" : ''}`} >
                 <span className={`${!isOpen ? styles.barOne : styles.barOneOpen} `}></span>
                 <span className={`${!isOpen ? styles.barTwo : styles.barTwoOpen} `}></span>
                 <span className={`${!isOpen ? styles.barThree : styles.barThreeOpen} `}></span>
